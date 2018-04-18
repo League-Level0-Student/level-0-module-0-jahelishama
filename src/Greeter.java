4250571b1549f66e4f71c13678d8646d951b0f84
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-17 19:27:22 -0700

    greter

---
+++ src/Greeter.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Greeter {
+	public static void main(String[] args) throws Exception {
+		String pen=JOptionPane.showInputDialog("What is your name?");
+		System.out.println(pen);
+	
+		JOptionPane.showMessageDialog(null, "Hi "+pen+"!");
+	
+	
+	
+	
+	
+	
+		
+	}	
+}
